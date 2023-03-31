6d330092a24a08fd0dbb1b7d3c11cc73d37fcd92
Jd1503 <jaideepdutta15@gmail.com>
2023-03-31 14:05:08 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "21032dd9d2mshbc7de221d47d130p196821jsnbaba755fef4d", // enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "455d1a63b32074044e2de3683e5b583e"; // enter your key from openweather API
